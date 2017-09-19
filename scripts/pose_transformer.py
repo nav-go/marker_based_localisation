267eb186bb8fd63c5810f42e99bad67231cc4e21
DroneAR <ngowrishankar@ucdavis.edu>
2017-09-18 18:44:55 -0700

    new launch files and scripts added

---
+++ scripts/pose_transformer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python  
+import roslib
+roslib.load_manifest('learning_tf')
+import rospy
+
+import tf
+from fiducial_msgs.msg import FiducialTransformArray
+
+def handle_camera_pose(msg):
+    br = tf.TransformBroadcaster()
+    trans = msg.transforms[0].transform.translation
+    rot = msg.transforms[0].transform.rotation
+    br.sendTransform((trans.x, trans.y, trans.z),
+                     (rot.x, rot.y, rot.z, rot.w),
+                     rospy.Time.now(),
+                     "solo1",
+                     "world")
+
+if __name__ == '__main__':
+    rospy.init_node('pose_transformer')
+    print "Started node"
+    
+    rospy.Subscriber('/fiducial_transforms',
+                     FiducialTransformArray,
+                     handle_camera_pose)
+    print "Subscribed to Pose"
+    rospy.spin()
